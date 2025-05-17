33709b4feeabc5e047efd88de055ce1169838c2d
Sebas Bautista Navarro <s_bautistanavarro@fanshaweonline.ca>
2025-05-17 12:23:37 -0400

    First triagle drawn

--- Aurora/src/Platform/OpenGL/OpenGLContext.cpp
+++ Aurora/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Aurora
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		AR_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		AR_CORE_INFO("OpenGL Info:");
+		AR_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		AR_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		AR_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 	void OpenGLContext::SwapBuffers()
 	{
